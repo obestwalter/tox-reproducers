48b8620ccab8633137c4078bc389f35e73f901bb
Oliver Bestwalter <oliver.bestwalter@avira.com>
2017-12-15 10:51:29 +0100

    not reproducing https://github.com/tox-dev/tox/issues/426 yet ...

--- setup.py
+++ setup.py
@@ -1,6 +1,3 @@
 from setuptools import setup
 
-setup(
-    name='testpackage',
-    version='0.1.0.dev0',
-)
+setup(name='tox-426')
---
+++ test_file.py
@@ -0,0 +1,3 @@
+def test_crash():
+    print("whatever")
+    assert 0
